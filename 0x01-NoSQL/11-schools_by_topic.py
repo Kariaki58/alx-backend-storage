c132247e12d9eadf7cd196dd86276337e378adc3
kariaki58 <programmerjavascript901@gmail.com>
2024-03-27 01:46:09 +0100

    function that returns the list of school having a specific topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""where can i learn python?"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """returns the list of school having a specific topic"""
+    return list(mongo_collection.find({"topic": topic}))
