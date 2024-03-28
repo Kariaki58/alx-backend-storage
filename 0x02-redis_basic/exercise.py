fc310ace1cd0c71e4510e9f1d41e10e4019e3bcb
kariaki58 <programmerjavascript901@gmail.com>
2024-03-28 03:46:52 +0100

    updating class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """writing strings to Redis"""
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 import uuid
 
 
@@ -16,5 +16,22 @@ class Cache:
     def store(self, data: Union[str, int, float, bytes]) -> str:
         """redis store"""
         hash = str(uuid.uuid4())
-        Cache._redis.set(hash, data)
+        self._redis.set(hash, data)
         return hash
+    
+    def get(
+        self, key: str, fn: Optional[Callable] = None
+        ) -> Union[str, bytes, int, float]:
+        """get key"""
+        data = self._redis.get(key)
+        if fn:
+            data = fn(data)
+        return data
+    
+    def get_str(self, key: str) -> str:
+        """return get string"""
+        return self.get(key, fn=str)
+
+    def get_int(self, key: str) -> int:
+        """return get int"""
+        return self.get(key, fn=int)
